8247554a912e0c4d1a62b7c6e37ed22b202206ce
mazin-abdelghany <68043318+mazin-abdelghany@users.noreply.github.com>
2022-04-11 15:43:26 -0700

    Complete JavaScript basics

--- basic-javascript/script.js
+++ basic-javascript/script.js
@@ -1,1 +1,54 @@
+// HELLO WORLD
 console.log("Hello, world!")
+
+// VARIABLES
+// Variable assignment 1
+let message1;
+message1 = 'Hello';
+
+// Variable assignment 2
+let message2 = 'Hello';
+
+// Declare multiple variables in one line
+
+// NOT RECOMMENDED
+let user1 = 'Mazin', age1 = 100, message3 = 'Hello';
+
+// Preferred
+let user2 = 'Mazin'
+let age2 = 100
+let message4 = 'Hello';
+
+// Some define variables like this:
+let user3 = 'Mazin',
+    age3 = 100,
+    message5 = 'Hello'
+
+// Some use comma first notation:
+let user4 = 'Mazin'
+    , age4 = 100
+    , message6 = 'Hello'
+
+// const declares a constant variable that cannot be changed
+const bday = '5 Sept 1890'
+
+// constants in JavaScript are often in all uppercase
+// most often, these are hard-coded values known before script run
+const GREEN = '#0F0'
+
+// ODIN EXERCISES
+console.log(23 + 97)
+console.log((4 + 6 + 9) / 77)
+
+let a = 10
+console.log(a)
+
+console.log(9*a)
+
+let b = 7 * a
+console.log(b)
+
+let MAX = 57
+let actual = MAX - 13
+let percentage = actual/MAX
+console.log(percentage)
